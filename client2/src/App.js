385d01fdd007c8fc487bec5725e075809fd8c3d3
yarden1032 <yarden1032@gmail.com>
2023-01-16 22:53:48 +0200

    dependencies issue3

---
+++ client2/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+ 
+import OptimizeForm from './PublicForm.js'
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <h1>Evolutionary Algorithms</h1>
+      </header>
+      
+<OptimizeForm/>
+    </div>
+  );
+}
+
+export default App;
